a1db8a616fd37e2dd754a82d845e6f7b778bc39b
RodrigoCBarbosa <carvalhorodrigobarbosa@gmail.com>
2025-12-02 23:13:27 -0300

    feat/livro_debug

--- controllers/livros_controller.py
+++ controllers/livros_controller.py
@@ -14,14 +14,20 @@ livro_service = LivroService()
 @login_required
 @view("livros/index")
 def listar():
+    # 1. CORREÇÃO: Capturar a sessão do Beaker aqui
+    session = request.environ.get('beaker.session')
+    
     livros = livro_service.listar()
-    return dict(title="Livros", livros=livros)
+    
+    # 2. CORREÇÃO: Enviar a 'session' para o template dentro do dicionário
+    return dict(title="Livros", livros=livros, session=session)
 
 
 # CRIAR LIVRO
 @livros_routes.post("/livros")
 @login_required
 def criar():
+    # Aqui não precisa de session pois fazemos redirect logo a seguir
     titulo = request.forms.get("titulo")
     autor = request.forms.get("autor")
     ano = request.forms.get("ano")
